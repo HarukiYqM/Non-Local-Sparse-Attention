8f6673de1fc84a892579dd86f92df576822ea129
HarukiYqM <meiyiqunapptr@163.com>
2021-06-15 02:29:15 +0800

    Delete final_run directory

--- final_run/data/sr291.py
+++
@@ -1,6 +0,0 @@
-from data import srdata
-
-class SR291(srdata.SRData):
-    def __init__(self, args, name='SR291', train=True, benchmark=False):
-        super(SR291, self).__init__(args, name=name)
-
